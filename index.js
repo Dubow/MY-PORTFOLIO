dbd58cc5ce54bba601749466a6870ee1b226872d
Abdirahman <abdirahmandubow4@gmail.com>
2024-02-01 15:38:47 +0800

    adding changes html,js,css

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ var tablinks = document.getElementsByClassName("tab-links")
         document.getElementById(tabname).classList.add("active-tab")
 
      }
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+   sidemeu.style.right = "0"
+}
+function closemenu(){
+   sidemeu.style.right = "-200px"
+}
